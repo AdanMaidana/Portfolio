f8ebeb919ccb7848862271975afe620e51aa40c8
Adan Maidana <adanmaidana31@gmail.com>
2024-06-19 22:50:10 -0300

    mejoras en las animaciones

--- README.md
+++
@@ -1,1 +0,0 @@
-Arreglar output: "serve
--- src/data/projects.js
+++ src/data/projects.js
@@ -13,17 +13,17 @@ export const projectsData = [
         "Utilizando la API de 'The Movie Database', puedes buscar miles de películas y series.",
         "Incluye pagina de bienvenida, inicio, secciones personalizadas, filtro de busqueda y una página de error que se muestra cuando intentas acceder a una dirección invalida."
       ],
-    skills: ["HTML, CSS, JavaScript, Bootstrap, React, Vite"],
+    skills: ["HTML", "CSS", "JavaScript", "Bootstrap", "React", "Vite"],
     web: "https://adanmaidana.github.io/Cinema/",
     repository: "https://github.com/AdanMaidana/Cinema",
   },
   {
-    title: "Domo-Challenge",
+    title: "Desafío DOMO",
     img: DomoChallengePreview,
     description: [
       "Prueba técnica realizada para DOMO en la que mediante una imagen debia replicar la página lo más parecido posible."
     ],
-    skills: ["HTML, CSS, JavaScript, Bootstrap, Astro"],
+    skills: ["HTML", "CSS", "JavaScript", "Bootstrap", "Astro"],
     web: "https://adanmaidana-domo-challenge.netlify.app/",
     repository: "https://github.com/AdanMaidana/DOMO-FrontEnd-Challenge",
   },
@@ -33,7 +33,7 @@ export const projectsData = [
     description: [
       "Realiza operaciones matemáticas básicas."
     ],
-    skills: ["HTML, CSS, JavaScript, React, Vite"],
+    skills: ["HTML", "CSS", "JavaScript", "React", "Vite"],
     web: "https://adanmaidana.github.io/Calculator/",
     repository: "https://github.com/AdanMaidana/Calculator",
   },
@@ -45,17 +45,17 @@ export const projectsData = [
         "Permite establecer sesiones de estudio, trabajo o cualquier actividad que necesites.",
         "Puedes personalizar la duración de las sesiones, los intervalos de descanso y al finalizar el contador sonará una alarma para avisarte que puedes tomarte un descanso."
       ],
-      skills: ["HTML, CSS, JavaScript, React, Vite"],
+    skills: ["HTML", "CSS", "JavaScript", "React", "Vite"],
     web: "https://adanmaidana.github.io/25-5-Clock/",
     repository: "https://github.com/AdanMaidana/25-5-Clock",
   },
   {
-    title: "Previsualizador-de-Markdown",
+    title: "Previsualizador de Markdown",
     img: Markdown,
     description: [
       "Utilizando un editor de texto que funciona con el lenguaje de marcado 'Markdown' puedes crear código HTML."
     ],
-    skills: ["HTML, CSS, JavaScript, React"],
+    skills: ["HTML", "CSS", "JavaScript", "React"],
     web: "https://adanmaidana.github.io/Markdown-Previewer/",
     repository: "https://github.com/AdanMaidana/Markdown-Previewer",
   },
